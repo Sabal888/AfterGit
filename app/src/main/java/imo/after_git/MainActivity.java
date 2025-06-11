5fcc82975421067cdb4c4f002a4d9baf18f06ec7
IMOitself <russjonn@gmail.com>
2025-06-11 16:57:22 +0800

    feat: run git status of a repository folder using Termux:API

--- app/src/main/java/imo/after_git/MainActivity.java
+++ app/src/main/java/imo/after_git/MainActivity.java
@@ -8,13 +8,23 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.Settings;
-import android.widget.EditText;
-import android.widget.Button;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import imo.after_run.TermuxUtils;
+import java.io.File;
 
 public class MainActivity extends Activity 
 {
+	File outputFile = new File("/storage/emulated/0/Download/.afterruntemp");
+	ViewGroup instruction;
+	TextView outputTxt;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -26,18 +36,46 @@ public class MainActivity extends Activity
             return;
 		}
 		
+		if(! TermuxUtils.hasTermuxPermission(this)){
+			TermuxUtils.requestTermuxPermission(this);
+			finish();
+			return;
+		}
+		
 		final EditText repoPathEdit = findViewById(R.id.repo_path_edittext);
 		final Button repoLoadBtn = findViewById(R.id.repo_load_btn);
+		instruction = findViewById(R.id.instruction);
+		outputTxt = findViewById(R.id.output_txt);
+		outputTxt.setMovementMethod(new ScrollingMovementMethod());
+		
+		instruction.setVisibility(View.GONE);
 		
 		repoLoadBtn.setOnClickListener(new View.OnClickListener(){
 			@Override
 			public void onClick(View v){
+				instruction.setVisibility(View.VISIBLE);
 				String repoPath = repoPathEdit.getText().toString().trim();
+				String command = "cd " + repoPath;
+				command += "\ngit status";
 				
-				//TODO: get the status of git repo using termux
+				IllegalStateException e = TermuxUtils.runCommand(command, outputFile, MainActivity.this);
+				if(e != null) handleIllegalStateException(e);
 			}
 		});
     }
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if(! outputFile.exists()) return;
+
+		String content = TermuxUtils.readCommandOutput(outputFile);
+
+		if(content.toString().trim().isEmpty()) return;
+
+		instruction.setVisibility(View.GONE);
+		outputTxt.setText(content.toString());
+	}
 	
 	
 	boolean hasStoragePermission() {
@@ -58,4 +96,27 @@ public class MainActivity extends Activity
             requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
         }
     }
+	
+	void handleIllegalStateException(Exception e){
+		final LinearLayout layout = new LinearLayout(this);
+		final TextView textView = new TextView(this);
+		final Button button = new Button(this);
+
+		layout.setOrientation(LinearLayout.VERTICAL);
+
+		textView.setText(e.getMessage());
+		textView.setTextIsSelectable(true);
+
+		button.setText("Maybe Open Termux:API first?");
+		button.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v){
+					Exception e = TermuxUtils.openTermuxAPI(MainActivity.this);
+					if (e != null) textView.setText(e.getMessage());
+				}
+			});
+		layout.addView(textView);
+		layout.addView(button);
+		setContentView(layout);
+	}
 }
