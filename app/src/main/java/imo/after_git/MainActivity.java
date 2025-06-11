fc24df0280fe925fb195be6f46c85a091b15e8f6
IMOitself <russjonn@gmail.com>
2025-06-11 14:53:00 +0800

    feat: request storage permission

--- app/src/main/java/imo/after_git/MainActivity.java
+++ app/src/main/java/imo/after_git/MainActivity.java
@@ -1,7 +1,13 @@
 package imo.after_git;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
 
 public class MainActivity extends Activity 
 {
@@ -10,5 +16,33 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		if(! hasStoragePermission()){
+            requestStoragePermission();
+            finish();
+            return;
+		}
+		
+		//TODO: get a dir with .git inside
+		
+    }
+	
+	
+	boolean hasStoragePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            return Environment.isExternalStorageManager();
+        } else {
+            return checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+        }
+    }
+
+    void requestStoragePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+            intent.setData(Uri.parse("package:" + getPackageName()));
+            startActivity(intent);
+        } else {
+			requestPermissions(new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+            requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+        }
     }
 }
